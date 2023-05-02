f7be6e266aaf625dd66da448ffbeb9140d802e39
Aidana-Asylbek-kyzy <aidanaasylbekkyzy12@gmail.com>
2023-05-02 15:10:24 +0600

    сделала дз и доп.

---
+++ main.js
@@ -0,0 +1,65 @@
+// var triangle = "*";
+
+// do {
+//     console.log(triangle);
+//     triangle += "*";
+// } while (triangle.length <= 7);
+
+// for (var i = 1; i <= 100; i++) {
+//     if ( !(i % 3) && !(i % 5) ) {
+//         console.log(i, "FizzBuzz");
+//     } else if ( !(i % 3) ) {
+//         console.log(i, "Fizz");
+//     } else if ( !(i % 5) ) {
+//         console.log(i, "Buzz");
+//     }
+// }
+
+// var deskSize = 8,
+//     totalSize = deskSize * deskSize,
+//     line = "",
+//     flag = true;
+
+
+// function createLine(deskSize, flag) {
+//     var result = "";
+//     for (var i = 0; i < deskSize; i++) {
+//         (i % 2) ? result += "#" : result += " ";
+//     }
+//     if (flag) {
+//         result = reverseString(result);
+//     }
+//     return result;
+// }
+
+// function reverseString(str) {
+//     var result = "";
+//     for (var i = str.length - 1; i > 0; i--) {
+//         result += str[i];
+//     }
+//     return result;
+// }
+
+// for (var i = 1; i <= deskSize; i++) {
+//     if (flag) {
+//         flag = false;
+//     } else {
+//         flag = true;
+//     }
+//     console.log(createLine(deskSize, flag));
+// }
+
+// const mass = ['Alex', 'Anna', 'Oleg']
+// const mass1 = [1, 2, 3]
+
+// const massiv = mass.concat(mass1);
+
+// console.log(massiv);
+
+// let array =  ['Алина', 'Саша', 'Алекс'];
+// console.log(array [0]);
+
+// let arr1 = [1, true, false, undefined];
+
+// arr1.splice(0, 2, 'Sasha', 'Alina');
+// console.log(arr1);
